466f7c420f8f5f7dd6b11d84d8a078f2b22e90d3
Matthew Alner <matthewalner@googlemail.com>
2021-02-08 20:36:09 +0000

    make clock page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClockPageComponent } from './clock-page/clock-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ClockPageComponent },
+  { path: 'clock', component: ClockPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
